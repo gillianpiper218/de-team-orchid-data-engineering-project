979b7681afdfcd5f1be15d4faa963388d8dff7ac
Anitaamena <143548989+Anitaamena@users.noreply.github.com>
2024-05-14 13:44:26 +0100

    remove cw_iam and revert changes

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -9,14 +9,6 @@ resource "aws_lambda_function" "ingestion_lambda" {
     role = aws_iam_role.ingestion_lambda_role.arn
     handler = "ingestion_lambda.lambda_handler"
     runtime = "python3.11"
-
-    layers        = [aws_lambda_layer_version.lambda_layer.arn]
-    depends_on    = [aws_cloudwatch_log_group.ingestion_lambda_log_group]
-
-    logging_config {
-    log_format = "Text"
-    }
-
   }
 
 data "archive_file" "lambda" {
