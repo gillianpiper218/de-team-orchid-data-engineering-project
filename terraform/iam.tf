19c6ff3014041192cc3bcee2dd2e2d45ce86fec7
Anitaamena <143548989+Anitaamena@users.noreply.github.com>
2024-05-14 13:56:30 +0100

    add iam policy and attachement for cw-ingestion lambda access

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -68,3 +68,33 @@ resource "aws_iam_role_policy_attachment" "attach_s3_get_access" {
 #     role = aws_iam_role.lambda_role.name
 #     policy_arn = aws_iam_policy.s3_ingestion_put_policy.arn
 # }
+
+data "aws_iam_policy_document" "cw_document" {
+  statement {
+
+    actions = [ "logs:CreateLogGroup" ]
+
+    resources = [
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
+    ]
+  }
+
+  statement {
+
+    actions = [ "logs:CreateLogStream", "logs:PutLogEvents" ]
+
+    resources = [
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.ingestion_lambda}:*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "cw_policy" {
+    name_prefix = "cw-policy-${var.ingestion_lambda}"
+    policy = data.aws_iam_policy_document.cw_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_cw_policy_attachment" {
+    role = aws_iam_role.ingestion_lambda_role.name
+    policy_arn = aws_iam_policy.cw_policy.arn
+}
