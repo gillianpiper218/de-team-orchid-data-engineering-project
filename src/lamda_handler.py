ba96351771b9bbf6a34030e8c59d2364538ce97d
ellen786 <140004543+ellen786@users.noreply.github.com>
2024-05-14 16:26:57 +0100

    lambda

---
+++ src/lamda_handler.py
@@ -0,0 +1,4 @@
+#  this contains the actual lambda code to upload to the lamda on amazon
+# will need to import the other two funcitons
+#  need a logger for if cannot injest properly
+# zip the file to upload
