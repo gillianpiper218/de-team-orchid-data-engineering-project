de51e546932c421293fb2604478b8deda0626bb2
gillianpiper218 <gillianpiper218@btinternet.com>
2024-05-17 14:33:01 +0100

    added do not destroy to ingestion bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,11 @@
 resource "aws_s3_bucket" "ingestion_s3_bucket" {
     bucket = "${var.ingestion_s3_bucket_name}"
+    lifecycle {
+    prevent_destroy = true
+}
 }
 
 # attribute of resource called life-cycle
 # if terraform destroy, don't get rid of historical data
+
+
