7b6647c796cbafc33fc2edff6e28c02c9eeeb995
04robik <04robik@gmail.com>
2024-05-20 15:40:02 +0100

    created processing lambda and test py files, outline all processing functions and first few tests

---
+++ src/processing_lambda.py
@@ -0,0 +1,82 @@
+import os
+import pandas as pd
+from datetime import datetime
+import logging
+import json
+from pprint import pprint
+import boto3
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+s3 = boto3.client('s3')
+current_time = datetime.now()
+
+INGESTION_S3_BUCKET_NAME = "de-team-orchid-totesys-ingestion"
+PROCESSED_S3_BUCKET_NAME = "de-team-orchid-totesys-processed"
+
+def get_object_key(prefix: str = None, table_name: str, bucket=INGESTION_S3_BUCKET_NAME) -> str:
+    # look in the given bucket with the prefix and get the object
+    # response = s3.list_objects_v2(**kwargs)
+    pass
+
+
+def remove_created_at_and_last_updated():
+    # remove created_at and last_updated keys function
+    # Assuming df is your DataFrame
+    # df.drop('column_name', axis=1, inplace=True)
+    pass
+
+
+def process_fact_sales_order():
+    # split created at date into created_date and created_time keys
+    # split last updated into created_date and created_time keys
+    remove_created_at_and_last_updated()
+    pass
+
+
+def process_dim_counterparty():
+    # remove commercial_contact and delivery_contact keys
+    remove_created_at_and_last_updated()
+    pass
+
+
+def process_dim_currency():
+    # create currency_name column
+    remove_created_at_and_last_updated()
+    pass
+
+
+def process_dim_date():
+    # create from each unique date
+    # - year
+    # - month
+    # - day
+    # - day_of_week (int type where Monday = 1 and Sunday = 7)
+    # - day_name
+    # - month_name
+    # - quarter
+    pass
+
+
+def process_dim_design():
+    remove_created_at_and_last_updated()
+    pass
+
+
+def process_dim_location():
+    # change address_id key into location_id
+    # Or rename the existing DataFrame (rather than creating a copy) 
+    # df.rename(columns={'oldName1': 'newName1', 'oldName2': 'newName2'}, inplace=True)
+    remove_created_at_and_last_updated()
+    pass
+
+
+def process_dim_staff():
+    remove_created_at_and_last_updated()
+    pass
+
+
+def convert_json_to_parquet():
+    # convert file format from json to parquet
+    pass
---
+++ tests/test_processing_lambda.py
@@ -0,0 +1,72 @@
+import pytest
+from moto import mock_aws
+import os
+import boto3
+from pg8000 import DatabaseError, InterfaceError
+from pprint import pprint
+from datetime import datetime
+import logging
+import json
+from src.processing_lambda import (
+    get_object_key,
+    remove_created_at_and_last_updated,
+    process_fact_sales_order,
+    process_dim_counterparty,
+    process_dim_currency,
+    process_dim_date,
+    process_dim_design,
+    process_dim_location,
+    process_dim_staff,
+    convert_json_to_parquet
+)
+
+LOGGER = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="function")
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "test"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    os.environ["AWS_SECURITY_TOKEN"] = "test"
+    os.environ["AWS_SESSION_TOKEN"] = "test"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+
+@pytest.fixture(scope="function")
+def s3(aws_credentials):
+    with mock_aws():
+        yield boto3.client("s3", region_name="eu-west-2")
+
+
+@pytest.fixture
+def bucket(s3):
+    s3.create_bucket(
+        Bucket="test_bucket",
+        CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+    )
+
+
+class TestGetObjectKey:
+    @pytest.mark.it("Unit test: returns object key form specified table in s3")
+    def test_return_object_key(self, s3):
+        pass
+
+    @pytest.mark.it("Unit test: raises exception for incorrect prefix")
+    def test_incorrect_prefix(self, s3):
+        pass
+
+    @pytest.mark.it("Unit test: raises exception for incorrect table name")
+    def test_incorrect_table_name(self, s3):
+        pass
+
+
+class TestRemoveCreatedAtAndLastUpdated:
+    @pytest.mark.it("Unit test: created_at key removed")
+    def test_remove_created_at(self, s3):
+        pass
+
+    @pytest.mark.it("Unit test: last_updated key removed")
+    def test_remove_last_updated(self, s3):
+        pass
+
