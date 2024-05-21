9cdad045909df5e2beb0e0787a3fb01de497923a
gillianpiper218 <gillianpiper218@btinternet.com>
2024-05-21 13:50:22 +0100

    updates to tf files

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -61,23 +61,23 @@ resource "aws_iam_policy" "lambda_layer_policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_layer_policy_attachment" {
-  role       = aws_iam_role.lambda_exec_role.name
+  role       = aws_iam_role.ingestion_function_role.name
   policy_arn = aws_iam_policy.lambda_layer_policy.arn
 }
 
-resource "aws_iam_role" "lambda_exec_role" {
-  name               = "lambda_exec_role"
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17",
-    Statement = [{
-      Effect    = "Allow",
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      },
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
+# resource "aws_iam_role" "lambda_exec_role" {
+#   name               = "lambda_exec_role"
+#   assume_role_policy = jsonencode({
+#     Version   = "2012-10-17",
+#     Statement = [{
+#       Effect    = "Allow",
+#       Principal = {
+#         Service = "lambda.amazonaws.com"
+#       },
+#       Action    = "sts:AssumeRole"
+#     }]
+#   })
+# }
 
 resource "aws_iam_policy" "secret_manager_policy" {
   name = "sm_access_permissions"
@@ -92,14 +92,14 @@ resource "aws_iam_policy" "secret_manager_policy" {
           "secretsmanager:GetSecretValue",
         ]
         Effect   = "Allow"
-        Resource = "arn:aws:secretsmanager:eu-west-2:851725259107:secret:totesys_environment-P0d7hT"
+        Resource = "*"
       },
     ]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "secret_manager_policy_attachment" {
-  role       = aws_iam_role.lambda_exec_role.name
+  role       = aws_iam_role.ingestion_function_role.name
   policy_arn = aws_iam_policy.secret_manager_policy.arn
 }
 
--- terraform/processing_lambda.tf
+++ terraform/processing_lambda.tf
@@ -1,32 +1,32 @@
-resource "aws_lambda_function" "processing_function" {
-    function_name = var.processing_function_name
-    filename = data.archive_file.processing_lambda.output_path
-    role = aws_iam_role.processing_function_role.arn
-    handler = "lambda-ellen.lambda_handler" #to confirm ingestion function name
-    depends_on    = [aws_cloudwatch_log_group.processing_function_log_group]
+# resource "aws_lambda_function" "processing_function" {
+#     function_name = var.processing_function_name
+#     filename = data.archive_file.processing_lambda.output_path
+#     role = aws_iam_role.processing_function_role.arn
+#     handler = "lambda-ellen.lambda_handler" #to confirm ingestion function name
+#     depends_on    = [aws_cloudwatch_log_group.processing_function_log_group]
 
-    runtime = "python3.11"
-  }
+#     runtime = "python3.11"
+#   }
 
-data "archive_file" "processing_lambda" {
-  type        = "zip"
-  source_file = "${path.module}/../src/lambda-ellen.py" #to confirm processing function name
-  output_path = "${path.module}/../function.zip"
-}
+# data "archive_file" "processing_lambda" {
+#   type        = "zip"
+#   source_file = "${path.module}/../src/lambda-ellen.py" #to confirm processing function name
+#   output_path = "${path.module}/../function.zip"
+# }
 
-resource "aws_s3_bucket_notification" "s3_ingestion_notification" {
-  bucket = aws_s3_bucket.ingestion_s3_bucket.bucket
+# resource "aws_s3_bucket_notification" "s3_ingestion_notification" {
+#   bucket = aws_s3_bucket.ingestion_s3_bucket.bucket
 
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.processing_function.arn
-    events              = ["s3:ObjectCreated:*"] 
-}
-}
+#   lambda_function {
+#     lambda_function_arn = aws_lambda_function.processing_function.arn
+#     events              = ["s3:ObjectCreated:*"] 
+# }
+# }
 
-resource "aws_lambda_permission" "allow_s3_to_invoke_processing_lambda" {
-  statement_id  = "AllowS3InvokeLambda"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.processing_function.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.ingestion_s3_bucket.arn 
-}
+# resource "aws_lambda_permission" "allow_s3_to_invoke_processing_lambda" {
+#   statement_id  = "AllowS3InvokeLambda"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.processing_function.function_name
+#   principal     = "s3.amazonaws.com"
+#   source_arn    = aws_s3_bucket.ingestion_s3_bucket.arn 
+# }
