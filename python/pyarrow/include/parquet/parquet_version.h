b0262db5cdc73701ebe0b40dbd4b4d884155e797
ellen786 <140004543+ellen786@users.noreply.github.com>
2024-05-23 13:55:16 +0100

    wip

--- python/pyarrow/include/parquet/parquet_version.h
+++ python/pyarrow/include/parquet/parquet_version.h
@@ -19,13 +19,13 @@
 #define PARQUET_VERSION_H
 
 #define PARQUET_VERSION_MAJOR 16
-#define PARQUET_VERSION_MINOR 1
+#define PARQUET_VERSION_MINOR 0
 #define PARQUET_VERSION_PATCH 0
 
-#define PARQUET_SO_VERSION "1601"
-#define PARQUET_FULL_SO_VERSION "1601.0.0"
+#define PARQUET_SO_VERSION "1600"
+#define PARQUET_FULL_SO_VERSION "1600.0.0"
 
 // define the parquet created by version
-#define CREATED_BY_VERSION "parquet-cpp-arrow version 16.1.0"
+#define CREATED_BY_VERSION "parquet-cpp-arrow version 16.0.0"
 
 #endif  // PARQUET_VERSION_H
