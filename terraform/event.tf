985589145beb2ba6fdca72ee8e3e2eef4422d74d
gillianpiper218 <gillianpiper218@btinternet.com>
2024-05-24 09:13:52 +0100

    added empty lambda handler

--- terraform/event.tf
+++ terraform/event.tf
@@ -5,11 +5,11 @@ resource "aws_cloudwatch_event_rule" "scheduler" {
 }
 
 
-# resource "aws_cloudwatch_event_rule" "processing_scheduler" {
-#     name = "processing-scheduler"
-#     description = "run processing lambda function every 7 minutes"
-#     schedule_expression = "rate(2 minutes)"
-# }
+resource "aws_cloudwatch_event_rule" "processing_scheduler" {
+    name = "processing-scheduler"
+    description = "run processing lambda function every 7 minutes"
+    schedule_expression = "rate(2 minutes)"
+}
 
 resource "aws_cloudwatch_event_target" "check_every_five_minutes" {
     rule = aws_cloudwatch_event_rule.scheduler.name
@@ -17,11 +17,11 @@ resource "aws_cloudwatch_event_target" "check_every_five_minutes" {
     arn = aws_lambda_function.ingestion_function.arn
 }
 
-# resource "aws_cloudwatch_event_target" "trigger_on_seven" {
-#     rule = aws_cloudwatch_event_rule.processing_scheduler.name
-#     target_id = "${var.processing_function_name}"
-#     arn = aws_lambda_function.processing_function.arn
-# }
+resource "aws_cloudwatch_event_target" "trigger_on_seven" {
+    rule = aws_cloudwatch_event_rule.processing_scheduler.name
+    target_id = "${var.processing_function_name}"
+    arn = aws_lambda_function.processing_function.arn
+}
 
 resource "aws_cloudwatch_log_group" "ingestion_function_log_group" {
   name              = "/aws/lambda/${var.ingestion_function_name}"
