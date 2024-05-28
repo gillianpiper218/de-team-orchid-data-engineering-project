9622c11c8fe47dd7750e8bb8a521b733673f57d8
04robik <04robik@gmail.com>
2024-05-28 16:22:40 +0100

    WIP for report queries

--- report_queries.sql
+++ report_queries.sql
@@ -1,3 +1,22 @@
--- top 3 design_names for each quarter (most popular design for each quarter)
+-- top 3 designs that sell the most units for each quarter of any year
+
+SELECT
+    dim_date.quarter,
+    dim_design.design_name,
+    fact_sales_order.units_sold
+    RANK() OVER(
+        PARTITION BY dim_date.quarter
+        ORDER BY fact_sales_order.units_sold DESC
+        )
+FROM dim_date
+JOIN fact_sales_order ON dim_date.date_id = fact_sales_order.created_date
+JOIN dim_design ON dim_design.design_id = fact_sales_order.design_id
+
 -- units sold * sales price for each day of the week (which day of the week would make the most sales)
--- agreed delivery location city 
+
+
+
+-- most popular design_name for each country (or city if there are minimal countries)
+-- biggest spend (units sold * sales price) per city
+-- for each each currency name, top 3 most popular design name
+-- bottom 3 counterparty legal names by country
