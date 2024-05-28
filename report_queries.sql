8c22685aaadaf0629aa2efc7b412d68bf4023de6
Souad-kh <Souad.alkhaledi@gmail.com>
2024-05-28 16:52:01 +0100

    writting sql queries

--- report_queries.sql
+++ report_queries.sql
@@ -13,10 +13,20 @@ JOIN fact_sales_order ON dim_date.date_id = fact_sales_order.created_date
 JOIN dim_design ON dim_design.design_id = fact_sales_order.design_id
 
 -- units sold * sales price for each day of the week (which day of the week would make the most sales)
-
-
+SELECT dim_date.day_of_week, SUM(fact_sales_order.units_sold*fact_sales_order.units_price) as total_sales
+FROM dim_date  
+JOIN fact_sales_order on dim_date.date_id = fact_sales_order.created_date
+GROUP BY dim_date.day_of_week
+ORDER BY total_sales DESC;
 
 -- most popular design_name for each country (or city if there are minimal countries)
+SELECT  dim_design.design_name
+FROM dim_country
+
+
+
+
+
 -- biggest spend (units sold * sales price) per city
 -- for each each currency name, top 3 most popular design name
 -- bottom 3 counterparty legal names by country
