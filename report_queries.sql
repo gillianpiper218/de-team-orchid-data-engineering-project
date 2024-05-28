6d94e5f220ae36606e4b6fc37f6a79507dff3e9c
04robik <04robik@gmail.com>
2024-05-28 14:24:59 +0100

    start to plan for required reports and queries

---
+++ report_queries.sql
@@ -0,0 +1,3 @@
+-- top 3 design_names for each quarter (most popular design for each quarter)
+-- units sold * sales price for each day of the week (which day of the week would make the most sales)
+-- agreed delivery location city 
